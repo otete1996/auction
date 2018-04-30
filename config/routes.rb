09bdce4f8f45c54ded8c8e64c943b3e522cb19f9
我妻　伶治 <otete1996@yahoo.co.jp>
2018-04-30 11:39:19 +0900

    add create,delete,update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/items/index" => "items#index"
+  get "/items/new" =>"items#new"
+  post "/items" => "items#create"
   get "items/:id" => "items#show"
+  get "items/:id/edit"=> "items#edit"
+  patch "/items/:id"=>"items#update"
+  delete "/items/:id"=>"items#destroy"
+
 
 end
